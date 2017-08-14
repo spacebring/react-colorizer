2519264d88c806000c076b515625a2840df58f8d
Rostyslav Khanas <rtkhanas@gmail.com>
2017-08-14 12:01:51 +0300

    Add margin for native component

--- src/components/ColorPicker/components-styled/ColorPickerWrapper.native.js
+++ src/components/ColorPicker/components-styled/ColorPickerWrapper.native.js
@@ -1,3 +1,5 @@
 import styled from 'styled-components/native';
 
-export default styled.View``;
+export default styled.View`
+  margin-bottom: 10;
+`;
--- storybooks/native/storybook/stories/index.js
+++ storybooks/native/storybook/stories/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dimensions } from 'react-native';
+import { Dimensions, View } from 'react-native';
 
 import { storiesOf } from '@storybook/react-native';
 import { action } from '@storybook/addon-actions';
@@ -10,10 +10,14 @@ const { width } = Dimensions.get('window');
 
 storiesOf('examples', module)
   .add('main', () => (
-    <ColorPicker
-      height={50}
-      color="#e56500"
-      width={width}
-      onColorChanged={action()}
-    />
+    <View>
+      <View style={{ backgroundColor: '#EDEBE6', height: 50 }} />
+      <ColorPicker
+        height={50}
+        color="#e56500"
+        width={width}
+        onColorChanged={action()}
+      />
+      <View style={{ backgroundColor: '#EDEBE6', height: 50 }} />
+    </View>
   ));
